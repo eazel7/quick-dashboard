1c3ea1c1b0de342e26c907a97ffeb08b2d9613e8
Diego Pérez <diego@zerovueltas.dev>
2023-11-29 09:38:20 +0100

    added websockets backend

---
+++ backend/index.mjs
@@ -0,0 +1,77 @@
+import http from "http";
+import { Server as SocketIOServer } from "socket.io";
+import config from "config";
+import internal from "stream";
+
+const updateIntervalInMilliseconds =
+  config.get("updateIntervalInSeconds") * 1000;
+
+function random(min, max) {
+  return min + Math.random() * (max - min);
+}
+
+function randomMeasurement() {
+  return {
+    deviation: random(0, 10),
+    deviationTolerance: random(0, 5),
+  };
+}
+
+function randomRows(numberOfRows) {
+  const array = Array(numberOfRows);
+
+  for (const index of array.keys()) {
+    array[index] = {
+      x: randomMeasurement(),
+      y: randomMeasurement(),
+      z: randomMeasurement(),
+      diameter: randomMeasurement(),
+    };
+  }
+
+  return array;
+}
+function createNewValues() {
+  return {
+    featureA: randomRows(6),
+    featureB: randomRows(1),
+    featureC: randomRows(1),
+    featureC: randomRows(1),
+    featureD: randomRows(1),
+    featureE: randomRows(3),
+  };
+}
+
+function updateRandomMeasurements(measuredValues) {
+  for (const key in measuredValues) {
+    for (const row of measuredValues[key]) {
+      for (const measurement in row) {
+        row[measurement].deviation = random(0, 10);
+      }
+    }
+  }
+}
+
+const measuredValues = createNewValues();
+
+setInterval(() => {
+  updateRandomMeasurements(measuredValues);
+}, config.updateRandomMeasurements);
+
+const server = http.createServer();
+const io = new SocketIOServer(server);
+io.on("connection", (client) => {
+  const interval = setInterval(() => {
+    client.emit("new-values", measuredValues);
+  }, updateIntervalInMilliseconds);
+
+  client.emit("new-values", measuredValues);
+
+  client.on("disconnect", () => {
+    clearInterval(internal);
+  });
+});
+
+const port = config.get("port");
+
+server.listen(port);
